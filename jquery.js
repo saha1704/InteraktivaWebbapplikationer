6846e4f77abb954c8b50fb12ce541b39d331d705
saha1704 <saha1704@student.miun.se>
2018-10-23 16:15:03 +0200

    footer på alla sidor

--- jquery.js
+++ jquery.js
@@ -8,3 +8,38 @@ $(document).ready(function(){
     });
 
 });
+
+/*Kanaler visa och gömma tablåer*/
+
+$(document).ready(function(){
+    $("#svt1").click(function(){
+        $('.day').hide();
+        $(svt1guide).show(150);
+        
+    });
+
+});
+
+$(document).ready(function(){
+    $("#tv3").click(function(){
+        $('.day').hide();
+        $(tv3guide).show(150);
+    });
+
+});
+
+$(document).ready(function(){
+    $("#kanal5").click(function(){
+        $('.day').hide();
+        $(kanal5guide).show(150);
+    });
+
+});
+
+$(document).ready(function(){
+    $("#tv6").click(function(){
+        $('.day').hide();
+        $(tv6guide).show(150);
+    });
+
+});
