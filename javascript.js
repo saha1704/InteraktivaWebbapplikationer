20c9e5999a89f62834c12ca92b614a96c5eb6964
saha1704 <saha1704@student.miun.se>
2018-10-25 15:11:33 +0200

    js

--- javascript.js
+++ javascript.js
@@ -1,22 +1,41 @@
- /*
- document.getElementById=("searchButton").onclick= function(){findprogram()};
 
  function findprogram(){
     
-    var x, userinput, ul, li, pg, i, time, returnmessage;
-    x = document.getElementById("searchtext");
-    userinput = x.value.toUpperCase();
-    ul = document.getElementsByClassName("guide");
-    li = ul.getElementsByTagName("li");
+    var form, userinput, uls, lis = new Array, programs = new Array, pg, i, time, returnmessage,;
+    form = document.getElementById("searchform")
+    userinput = form.searchstring.value.toUpperCase();
+/*     userinput = document.getElementById("searchtext").value.toUpperCase();
+    
+    uls = document.getElementsByClassName("guide");*/
+
+    var ul = document.getElementById("rapport");
+    var items = ul.getElementsByTagName("li");
+
+    for (i = 0; i < items.length; i++){
+        items[i].value
+
+    }
 
 
-    for ( i = 0; i < li.length; i++){
-        pg = li[i].getElementsByClassName("programtitle")[0];
+
+
+
+
+
+
+        /*
+
+    for (i = 0; i < uls.length; i++){
+        var x = uls[i].getElementsByTagName("li");
+        lis.push(x);
+    }
+     for ( i = 0; i < lis.length; i++){
+        pg = lis[i].getElementsByClassName("programtitle")[0];
         if (pg.innerHTML.toUpperCase().indexOf(userinput) > -1) {
-            time = li[i].getElementsByClassName("time")
+            time = lis[i].getElementsByClassName("time")
             returnmessage = pg + " " + time;
             alert(returnmessage);
         }
-    }
+    } */
     
-}*/
+}
---
+++ jquery.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $("#menuicon").click(function(){
+        $(mobilemenulist).show(150);
+    });
+
+    $("#exitmenuicon").click(function(){
+        $(mobilemenulist).hide();
+    });
+
+});
+
+/*Kanaler visa och gömma tablåer*/
+
+$(document).ready(function(){
+    $("#svt1").click(function(){
+        $('.day').hide();
+        $(svt1guide).show(150);
+        
+    });
+
+    $("#tv3").click(function(){
+        $('.day').hide();
+        $(tv3guide).show(150);
+    });
+
+    $("#kanal5").click(function(){
+        $('.day').hide();
+        $(kanal5guide).show(150);
+    });
+
+    $("#tv6").click(function(){
+        $('.day').hide();
+        $(tv6guide).show(150);
+    });
+
+
+});
+
+$(function(){
+
+    var data = ["Rapport","Antikrundan","Jönssonligan & dynamit-Harry","Svenska Hollywoodfruar", "Efterlyst","How I Met Your Mother","Arga snickaren","Breaking News med Filip & Fredrik", "Ex on the beach Sverige","The Simpsons","2 1/2 men","Erik och mackan"];
+    $("searchText").autocomplete({source: data})
+});
